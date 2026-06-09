832c5be5020e994106fb1d0467c127d50922c7f5
Kommu Device <device@kommu.ai>
2026-06-09 04:58:38 +0000

    Disable Updater

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -108,7 +108,7 @@ procs = [
   PythonProcess("radard", "selfdrive.controls.radard", only_onroad),
   PythonProcess("hardwared", "system.hardware.hardwared", always_run),
   PythonProcess("tombstoned", "system.tombstoned", always_run, enabled=not PC),
-  PythonProcess("updated", "system.updated.updated", only_offroad, enabled=not PC),
+  #PythonProcess("updated", "system.updated.updated", only_offroad, enabled=not PC),
   NativeProcess("uploader", ".", ["python3", "-m", "system.loggerd.uploader"], always_run),
   PythonProcess("statsd", "system.statsd", always_run, enabled=TICI),
   PythonProcess("feedbackd", "selfdrive.ui.feedback.feedbackd", only_onroad),
